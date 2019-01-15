515f0002a3b16dca2749afa683f4dff3561e2daa
Andy Wu <bwu2sfu@gmail.com>
2019-01-15 15:37:49 -0800

    first commit

---
+++ StopServicesAndInstall.ps1
@@ -0,0 +1,7 @@
+﻿$batFile = "C:\Learning\PowerShell\temp\Install-rev-$rev.bat"
+New-Item $batFile -type file -force
+
+Add-Content $batFile "net stop `"Your Service 1`""
+Add-Content $batFile "net stop `"Your Service 2`""
+Add-Content $batFile ""
+Add-Content $batFile "e:\deployment\Setup.exe"
---
+++ temp/Install-rev-.bat
@@ -0,0 +1,4 @@
+net stop "Your Service 1"
+net stop "Your Service 2"
+
+e:\deployment\Setup.exe
